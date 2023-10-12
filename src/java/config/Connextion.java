d184304cd05e4bab69a07c50b7012d925efa0a97
Joel Huaman <joelhuaman7m@gmail.com>
2023-10-12 07:55:28 -0500

    fix/connection database 1.1

--- src/java/config/Connextion.java
+++ src/java/config/Connextion.java
@@ -8,8 +8,8 @@ public class Connextion {
     public Connection getConnextion() {
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
-            //Connection conexion = DriverManager.getConnection("jdbc:mysql://51.89.190.173:3306/piad515g3?serverTimezone=UTC", "piad515", "senati2023");
-            Connection conexion = DriverManager.getConnection("jdbc:mysql://bghbeqw5xiyxve0vvbs8-mysql.services.clever-cloud.com:3306/bghbeqw5xiyxve0vvbs8", "uypkizie0yqjj6hf", "EAewIMRVrQmV3HjwFpxt");
+            Connection conexion = DriverManager.getConnection("jdbc:mysql://100.66.11.205:3306/mechanical-workshop?serverTimezone=UTC", "root", "OPXtyb23199");
+            //Connection conexion = DriverManager.getConnection("jdbc:mysql://bghbeqw5xiyxve0vvbs8-mysql.services.clever-cloud.com:3306/bghbeqw5xiyxve0vvbs8", "uypkizie0yqjj6hf", "EAewIMRVrQmV3HjwFpxt");
             //Connection conexion = DriverManager.getConnection("jdbc:mysql://localhost:3306/machanical-workshop?serverTimezone=UTC", "root", "");
             System.out.println("successful connection!!");
             return conexion;
